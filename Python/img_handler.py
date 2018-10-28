8741d2e878d76da2527e4498afbbad1996cf02bd
Patsakula Nikita <nicman2222@gmail.com>
2018-10-29 03:46:41 +0700

    WIP: filename attribute added, async list comprehension partly done.

--- Python/img_handler.py
+++ Python/img_handler.py
@@ -1,4 +1,5 @@
 from Python import img_handler_functions as imf
+import multiprocessing as mulpro
 
 # Service directory:
 tmp = "./TMP"
@@ -12,15 +13,27 @@ imf.script_init()
 # Get list of images:
 image_list = imf.get_image_list(dir=path)
 
+# Test output:
+list(map(lambda x: print(x.filename), image_list))
+
 # Create service directory:
 imf.create_directory(tmp)
 
-# Enhance images:
-image_list = list(map(lambda x: imf.enchance(image=x), image_list))
+# Achtung!
+pool = mulpro.Pool(processes=mulpro.cpu_count())
 
+image_list = list(pool.map(imf.enhance, image_list))
+
+pool.close()
+# Achtung! end
+
+# Sync stable:
+# image_list = list(map(lambda x: imf.enhance(image=x), image_list))
+
+# if image list not empty
 if image_list:
     # Writing images in service directory:
-    image_list = list(map(lambda x: imf.wite_image(image=x, number=image_list.index(x), path=tmp), image_list))
+    image_list = list(map(lambda x: imf.wite_image(image=x, path=tmp), image_list))
     # Creating PDF:
     imf.create_pdf(sources_path=tmp, result_path=path)
 else:
@@ -30,4 +43,4 @@ else:
 imf.delete_directory(path=tmp)
 
 # Compress sources to archive:
-imf.compress_files(path=path)
+# imf.compress_files(path=path)
--- Python/img_handler_functions.py
+++ Python/img_handler_functions.py
@@ -3,6 +3,8 @@ from PIL import Image
 import img2pdf
 import zipfile
 import shutil
+import math
+import gc
 import os
 
 SUCCESS = '\033[92m' + "[Success]" + '\033[0m'
@@ -20,12 +22,11 @@ def script_init():
 
 def get_image_list(dir):
     print(INFO + "Start scanning the directory!")
-    dir_files = [f for f in os.listdir(dir) if os.path.isfile(f)]
+    dir_files = [f for f in os.listdir(dir) if os.path.isfile(f) and f.endswith(tuple(image_ext))]
+    dir_files.sort()
     result = []
     try:
-        for filename in dir_files:
-            if filename.endswith(tuple(image_ext)):
-                result.append(Image.open(filename))
+        result = [Image.open(dir + filename) for filename in dir_files]
     except result == 0:
         print(ERROR + "There are not matching files in the directory!")
         exit(1)
@@ -56,8 +57,9 @@ def delete_directory(path):
         print(INFO + "The service directory successfully deleted!")
 
 
-def enchance(image, contrast=2.0, color=0.0, brightness=1.0, shape=2.0):
+def enhance(image, contrast=2.0, color=0.0, brightness=1.0, shape=2.0):
     try:
+        print(image.filename)
         # Color enhance:
         enhancer = ench.Color(image)
         enhanced = enhancer.enhance(color)
@@ -70,22 +72,26 @@ def enchance(image, contrast=2.0, color=0.0, brightness=1.0, shape=2.0):
         # Shape enhance:
         enhancer = ench.Sharpness(enhanced)
         enhanced = enhancer.enhance(shape)
+        enhanced.filename = image.filename
     except:
         print(ERROR + "Enhance error!")
         shutil.rmtree("./TMP", ignore_errors=True)
-        exit(1)
+        # exit(1)
     else:
+        gc.collect()
         return enhanced
 
 
-def wite_image(image, number, path):
-    image.save(path + "/" + "scan_" + str(number) + ".png", "PNG")
+def wite_image(image, path):
+    image.save(path + "/" + image.filename, "PNG")
     return image
 
 
 def create_pdf(sources_path, result_path):
     print(INFO + "Start generating PDF!")
-    pdf_bytes = img2pdf.convert([sources_path + "/" + i for i in os.listdir(sources_path) if i.endswith(".png")])
+    file_list = [sources_path + "/" + i for i in os.listdir(sources_path) if i.endswith(".png")]
+    file_list.sort()
+    pdf_bytes = img2pdf.convert(file_list)
     result_file = open(result_path + "/uncompressed.pdf", "wb")
     result_file.write(pdf_bytes)
     print(SUCCESS + "Uncompressed PDF created!")
@@ -115,3 +121,25 @@ def compress_files(path):
             print(ERROR + "Images removing error!")
         else:
             print(SUCCESS + "Images removed!")
+
+def enum(iter, start=1):
+    n = start
+    for i in iter:
+        yield n, i
+        n += 1
+
+
+def slice_image(image, slice_num=2):
+    wight, height = image.size
+
+    upper = 0
+
+    slices = int(math.ceil(height / slice_num))
+
+    for i,slice in enum(range(slice_num)):
+        left = 0;
+        upper = upper
+        if i == slice_num:
+            lower = height
+        else:
+            lower = int(i)
